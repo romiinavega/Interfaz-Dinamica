e8b25c38454a6c30a19e07160e15e7c604b1c64b
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-30 12:55:03 -0700

    Agregar archivos de proyecto.

---
+++ Interfaz Dinamica/ParametrosAlumno.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interfaz_Dinamica
+{
+    /// <summary>
+    /// Lógica de interacción para ParametrosAlumno.xaml
+    /// </summary>
+    public partial class ParametrosAlumno : UserControl
+    {
+        public ParametrosAlumno()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
